Added mpu9250 libs, corrected declarations of __init__, added methods for Euler angles and quaternions. Issue with installing numpy via pip on Pi still persists.

src/imu.py
@@ -1,19 +1,34 @@
 # src/imu.py
 import board, busio, time, math
-from adafruit_mpu9250 import MPU9250
+from mpu9250 import MPU9250
 import imufusion
 import numpy as np
 
 class IMUReader:
-    def __init__(self, i2c_bus, address=0x68):
+    def __init__(self, i2c_bus, address, sample_rate=100):
         self.imu = MPU9250(i2c_bus, address=address)
         self.ahrs = imufusion.Ahrs()
+        self.sample_rate = sample_rate  # Hz
         time.sleep(0.5)
 
+    def update(self):
+        # Read sensor data
+        gyro = np.array(self.imu.gyro)  # degrees/s
+        accel = np.array(self.imu.acceleration)  # g
+        mag = np.array(self.imu.magnetic)  # uT
+
+        # Feed data to AHRS
+        self.ahrs.update(gyro, accel, mag, 1.0 / self.sample_rate)
+
     def get_euler(self):
-        ax, ay, az = self.imu.acceleration
-        roll  = math.degrees(math.atan2(ay, az))
-        pitch = math.degrees(math.atan2(-ax, math.sqrt(ay*ay+az*az)))
-        mx, my, mz = self.imu.magnetic
-        yaw   = math.degrees(math.atan2(my, mx))
-        return (roll, pitch, yaw)
+        # Call update before getting orientation
+        self.update()
+        # Returns (roll, pitch, yaw) in degrees
+        return tuple(self.ahrs.quaternion.to_euler())
+
+    def get_quaternion(self):
+        # Call update before getting orientation
+        self.update()
+        # Returns quaternion as (w, x, y, z)
+        q = self.ahrs.quaternion
+        return (q.w, q.x, q.y, q.z)